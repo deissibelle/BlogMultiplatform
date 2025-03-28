41a2771633a2d0c5e0c0ef06406e02f2bb9c5956
deissibelle <deissibelle@gmail.com>
2025-03-28 16:31:01 +0100

    admin panel

---
+++ site/build.gradle.kts
@@ -0,0 +1,69 @@
+import com.varabyte.kobweb.gradle.application.util.configAsKobwebApplication
+import kotlinx.html.link
+import kotlinx.html.script
+
+plugins {
+    alias(libs.plugins.kotlin.multiplatform)
+    alias(libs.plugins.compose.compiler)
+    alias(libs.plugins.kobweb.application)
+    alias(libs.plugins.serialization.plugin)
+    // alias(libs.plugins.kobwebx.markdown)
+}
+
+group = "com.example.blogmultiplatform"
+version = "1.0-SNAPSHOT"
+
+kobweb {
+    app {
+        index {
+            description.set("Powered by Kobweb")
+
+            head.add {
+                script {
+                    src = "/highlight.min.js"
+                }
+                link {
+                    rel = "stylesheet"
+                    href = "/github-dark.css"
+                }
+                script {
+                    src = "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+                }
+                link {
+                    rel = "stylesheet"
+                    href = "https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css"
+                }
+            }
+        }
+    }
+}
+
+kotlin {
+    configAsKobwebApplication("blogmultiplatform", includeServer = true)
+
+    sourceSets {
+//        commonMain.dependencies {
+//          // Add shared dependencies between JS and JVM here
+//        }
+        jsMain.dependencies {
+            implementation(libs.compose.runtime)
+            implementation(libs.compose.html.core)
+            implementation(libs.kobweb.core)
+            implementation(libs.kobweb.silk)
+            implementation(libs.silk.icons.fa)
+            implementation(libs.kotlinx.serialization)
+            // implementation(libs.kobwebx.markdown)
+
+        }
+        jvmMain.dependencies {
+            implementation(libs.kmongo.database)
+            implementation(libs.kmongo)
+
+            implementation(libs.kmongo.serialization)
+            implementation(libs.kobwebx.serialization.kotlinx)
+            implementation(libs.kotlinx.serialization)
+            implementation(libs.slf4j.simple)
+            compileOnly(libs.kobweb.api) // Provided by Kobweb backend at runtime
+        }
+    }
+}
---
+++ site/src/commonMain/kotlin/com/example/blogmultiplatform/models/User.kt
@@ -0,0 +1,15 @@
+@file:Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
+
+package com.example.blogmultiplatform.models
+
+
+expect class User {
+    val _id: String
+    val username: String
+    val password: String
+}
+
+expect class UserWithoutPassword {
+    val _id: String
+    val username: String
+}
