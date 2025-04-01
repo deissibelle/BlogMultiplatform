407465214d441d39276b5890470e9f5b3ea194c8
deissibelle <deissibelle@gmail.com>
2025-04-01 18:16:58 +0200

    category dropdown component

---
+++ site/src/commonMain/kotlin/com/example/blogmultiplatform/models/Category.kt
@@ -0,0 +1,9 @@
+@file:Suppress("EXPECT_ACTUAL_CLASSIFIERS_ARE_IN_BETA_WARNING")
+
+package com.example.blogmultiplatform.models
+
+ expect enum class Category {
+    Technology,
+    Programming,
+    Design
+}
--- site/src/jsMain/kotlin/com/example/blogmultiplatform/pages/admin/Create.kt
+++ site/src/jsMain/kotlin/com/example/blogmultiplatform/pages/admin/Create.kt
@@ -17,11 +17,15 @@ import com.varabyte.kobweb.compose.foundation.layout.Row
 import com.varabyte.kobweb.compose.ui.Alignment
 import com.example.blogmultiplatform.util.Id
 import com.example.blogmultiplatform.util.noBorder
-import com.varabyte.kobweb.compose.style.KobwebComposeStyleSheet.attr
+import com.varabyte.kobweb.compose.css.Cursor
 import com.varabyte.kobweb.compose.ui.Modifier
+import com.varabyte.kobweb.compose.ui.attrsModifier
+import com.varabyte.kobweb.compose.ui.graphics.Colors
 import com.varabyte.kobweb.compose.ui.modifiers.backgroundColor
 import com.varabyte.kobweb.compose.ui.modifiers.borderRadius
+import com.varabyte.kobweb.compose.ui.modifiers.classNames
 import com.varabyte.kobweb.compose.ui.modifiers.color
+import com.varabyte.kobweb.compose.ui.modifiers.cursor
 import com.varabyte.kobweb.compose.ui.modifiers.fillMaxSize
 import com.varabyte.kobweb.compose.ui.modifiers.fillMaxWidth
 import com.varabyte.kobweb.compose.ui.modifiers.fontFamily
@@ -33,8 +37,9 @@ import com.varabyte.kobweb.compose.ui.modifiers.padding
 import com.varabyte.kobweb.core.Page
 import org.jetbrains.compose.web.dom.Input
 import com.varabyte.kobweb.compose.ui.modifiers.id
+import com.varabyte.kobweb.compose.ui.modifiers.onClick
 import org.jetbrains.compose.web.dom.Ul
-
+import com.example.blogmultiplatform.models.Category
 import com.varabyte.kobweb.compose.ui.toAttrs
 import com.varabyte.kobweb.silk.components.forms.Switch
 import com.varabyte.kobweb.silk.components.forms.SwitchSize
@@ -45,8 +50,9 @@ import com.varabyte.kobweb.silk.style.breakpoint.Breakpoint
 import com.varabyte.kobweb.silk.theme.breakpoint.rememberBreakpoint
 import org.jetbrains.compose.web.attributes.InputType
 import org.jetbrains.compose.web.css.px
-
-
+import org.jetbrains.compose.web.dom.A
+import org.jetbrains.compose.web.dom.Li
+import org.jetbrains.compose.web.dom.Text
 
 
 data class CreatePageUiState(
@@ -56,7 +62,7 @@ data class CreatePageUiState(
     var thumbnail: String = "",
     var thumbnailInputDisabled: Boolean = true,
     var content: String = "",
-//    var category: Category = Category.Programming,
+    var category: Category = Category.Programming,
     var buttonText: String = "Create",
     var popular: Boolean = false,
     var main: Boolean = false,
@@ -72,7 +78,7 @@ data class CreatePageUiState(
         subtitle = "",
         thumbnail = "",
         content = "",
-//        category = Category.Programming,
+        category = Category.Programming,
         buttonText = "Create",
         main = false,
         popular = false,
@@ -213,9 +219,89 @@ fun CreateScreen(){
                             attr("value", uiState.subtitle)
                         }
                 )
+                CategoryDropdown(
+                    selectedCategory = uiState.category,
+                    onCategorySelect = { uiState = uiState.copy(category = it) }
+                )
+                Row(
+                    modifier = Modifier.fillMaxWidth().margin(topBottom = 12.px),
+                    horizontalArrangement = Arrangement.Start,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Switch(
+                        modifier = Modifier.margin(right = 8.px),
+                        checked = !uiState.thumbnailInputDisabled,
+                        onCheckedChange = { uiState = uiState.copy(thumbnailInputDisabled = !it) },
+                        size = SwitchSize.MD
+                    )
+                    SpanText(
+                        modifier = Modifier
+                            .fontSize(14.px)
+                            .fontFamily(FONT_FAMILY)
+                            .color(Theme.HalfBlack.rgb),
+                        text = "Paste an Image URL instead"
+                    )
+                }
             }
 
         }
 
+    }
+}
+@Composable
+fun CategoryDropdown(
+    selectedCategory: Category,
+    onCategorySelect: (Category) -> Unit
+) {
+    Box(
+        modifier = Modifier
+            .margin(topBottom = 12.px)
+            .classNames("dropdown")
+            .fillMaxWidth()
+            .height(54.px)
+            .backgroundColor(Theme.LightGray.rgb)
+            .cursor(Cursor.Pointer)
+            .attrsModifier {
+                attr("data-bs-toggle", "dropdown")
+            }
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxSize()
+                .padding(leftRight = 20.px),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            SpanText(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .fontSize(16.px)
+                    .fontFamily(FONT_FAMILY),
+                text = selectedCategory.name
+            )
+            Box(modifier = Modifier.classNames("dropdown-toggle"))
+        }
+        Ul(
+            attrs = Modifier
+                .fillMaxWidth()
+                .classNames("dropdown-menu")
+                .toAttrs()
+        ) {
+            Category.entries.forEach { category ->
+                Li {
+                    A(
+                        attrs = Modifier
+                            .classNames("dropdown-item")
+                            .color(Colors.Black)
+                            .fontFamily(FONT_FAMILY)
+                            .fontSize(16.px)
+                            .onClick { onCategorySelect(category) }
+                            .toAttrs()
+                    ) {
+                        Text(value = category.name)
+                    }
+                }
+            }
+        }
     }
 }
